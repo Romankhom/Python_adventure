4c4f7e742ef07d7c61f30373bf23d5105650f014
Khomenko Roman <homenko.r.v@gmail.com>
2024-11-25 22:37:51 +0100

    chpt2

---
+++ Python_adventure_chpt2.py
@@ -0,0 +1,18 @@
+drinks = ["chokolate","coffie","decadf"]
+flavors = ["caramel","vanilla","peppermint","fruitmint","raspberry","plain"]
+toppings = ["chocolate","cinnamon","caramel"]
+# print (drinks)
+i = 1
+for d in drinks:
+    print (i, d)
+    i = i+1
+    
+i = 1   
+for f in flavors:
+    print (i, f)
+    i = i + 1
+
+i = 1
+for top in toppings:
+    print (i, top)
+    i = i + 1
