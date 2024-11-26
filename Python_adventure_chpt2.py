ba6555661e4475b4fdedb7b7fedc8634a10bcb7f
Khomenko Roman <homenko.r.v@gmail.com>
2024-11-26 22:59:03 +0100

    26/112024

--- Python_adventure_chpt2.py
+++ Python_adventure_chpt2.py
@@ -1,18 +1,27 @@
 drinks = ["chokolate","coffie","decadf"]
 flavors = ["caramel","vanilla","peppermint","fruitmint","raspberry","plain"]
 toppings = ["chocolate","cinnamon","caramel"]
+print ("Valeriia CAFE menu")
+print ("Onze drankjes")
+print ("-------------")
+
 # print (drinks)
 i = 1
 for d in drinks:
     print (i, d)
     i = i+1
-    
+drink = input ("Kies je drankje: ")
+print ("Onze smaaktoepassingen")
+print ("----------------------")  
 i = 1   
 for f in flavors:
     print (i, f)
     i = i + 1
-
+flavor = input ("Kies je smaaktoepassingen: ")
+print ("Onze toppings")
+print ("-------------")
 i = 1
 for top in toppings:
     print (i, top)
     i = i + 1
+topping = input ("Kies je topping: ")
